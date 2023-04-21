e7336c09af26f1d68a7c5d2694370b0f675654f3
RextVPN <117340359+RextVPN@users.noreply.github.com>
2023-04-21 20:50:56 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+#auto insatller
+
+wget https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/xray-mod/xray2.sh && chmod +x xray2.sh && sed -i -e 's/\r$//' xray2.sh && screen -S xray2.sh ./xray2.sh
+
---
+++ plugin-panel-ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#add akun SSH
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-ssh.sh" && chmod +x /usr/local/bin/add-ssh-user
+wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-ssh.sh" && chmod +x /usr/local/bin/del-ssh-user
